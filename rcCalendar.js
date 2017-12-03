c1d88e55b6a27e35397c52e66acc328f9f30a5e3
Demonshae Glover <dshaeglover@gmail.com>
2017-12-03 18:43:56 -0500

    adding changes made to css, html, and js file

--- rcCalendar.js
+++ rcCalendar.js
@@ -1,5 +1,19 @@
 
 function doFirst(){
+    pics = document.getElementById("names");
+    pics.addEventListener("dragstart", startDrag, false);
+    categories = document.getElementById("categories");
+    categories.addEventListener("dragenter", function(e){e.preventDefault();},false);
+    categories.addEventListener("dragover", function(e){e.preventDefault();},false);
+    categories.addEventListener("drop", dropped, false);
+}
 
+function startDrag(e){
+   var code = '<table id="names" border="1"></table>';
+   e.dataTransfer.setData('Text', code);
+}
+function dropped(e){
+  e.preventDefault();
+  categories.innerHTML = e.dataTransfer.getData('Text');
 }
 window.addEventListener("load", doFirst, false);
